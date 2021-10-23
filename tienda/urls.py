f6fd0eb3e383070b16557e30e8d9e88ef1b224c9
Marigrace Silva :) <50328933+ks161297@users.noreply.github.com>
2021-10-23 08:01:08 -0500

    agregue ruta

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include,url
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
-schema_view = get_schema_view(API, renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+schema_view = get_schema_view(title='API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
 urlpatterns = [
     path('handmade/', include('handmade.urls')),
     path('facturacion/', include('facturacion.urls')),
