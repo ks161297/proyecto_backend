c64431578d6717f9c1259e6df0525a235b095120
Marigrace Silva :) <50328933+ks161297@users.noreply.github.com>
2021-10-28 11:19:41 -0500

    chore: avances 2

--- handmade/views.py
+++ handmade/views.py
@@ -44,6 +44,7 @@ class ClientesController(RetrieveAPIView):
 class ClienteController(RetrieveUpdateAPIView):
     serializer_class = clienteSerializer
     queryset = ClienteModel.objects.all()
+    lookup_field = 'members'
 
     def get(self, request:Request, id):
         clienteEncontrado = self.get_queryset().filter(clienteId = id).first()
